67947e1a60149f298d2f216569d0b8f4c49e36ad
Shashi Kumar CD <shashikumar.chandanaduru.devarajappa@volvo.com>
2021-05-02 15:33:09 +0530

    Added 2nd Test

--- WebAutomationFramework/src/test/java/tests/FirstTest.java
+++ WebAutomationFramework/src/test/java/tests/FirstTest.java
@@ -3,7 +3,12 @@ package tests;
 import org.testng.annotations.Test;
 
 public class FirstTest {
-	@Test
+	@Test(priority=1)
+	public void Test() {
+		System.out.println("First test");
+	}
+	
+	@Test(priority=2)
 	public void Test1() {
 		System.out.println("First test");
 	}
