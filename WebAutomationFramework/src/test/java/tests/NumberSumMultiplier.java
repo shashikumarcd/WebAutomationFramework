205d0301aeee671d16c14b2272dfe2cf76eb759a
Shashi Kumar CD <shashikumar.chandanaduru.devarajappa@volvo.com>
2021-05-03 08:10:19 +0530

    Updated NumberSumMultiplier and renamed README.md to README

--- WebAutomationFramework/src/test/java/tests/NumberSumMultiplier.java
+++ WebAutomationFramework/src/test/java/tests/NumberSumMultiplier.java
@@ -4,38 +4,36 @@ import java.util.Scanner;
 
 public class NumberSumMultiplier {
 
-	public static void main(String[] args) {		
+	
+	/* Add sum of the array list, if grater than add sum * 2 then display true */
 		
+	public static void main(String[] args) {
 		int[] arr = {1,2,4,1,1,2,3};
 		System.out.println(SumMultiplier(arr));
-
 	}
-		
+
 	static boolean SumMultiplier(int arr[]) {		
-		
+
 		int largest = arr[0];
-		int secLargest = arr[0];
-		
+		int secLargest = arr[1];
 		int sum = 0;
 		int n =arr.length;
 		for(int i =0;i<n;i++) {
-			
-			if(arr[i]>largest)
-			{
-			largest=arr[i];
+
+			if(largest<arr[i])
+			{			
+				secLargest=largest;
+				largest=arr[i];			
 			}
-			if(secLargest >arr[i]) 
-			{
-				secLargest=arr[i];
-			}			
+			else if(secLargest < arr[i]){
+				secLargest = arr[i];
+	        }
 			sum = sum + arr[i];
 		}
-		
+
 		if((sum * 2)>largest *secLargest)
 			return false;
 		else
-			return true;	
-		
+			return true;
 	}
-
 }
